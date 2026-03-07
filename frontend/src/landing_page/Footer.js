0c4c091d25f972baaa9dbbbffa162f5b8453f895
Faizaan <iamrevenent007@gmail.com>
2026-03-08 00:27:19 +0530

    Home page done

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -2,7 +2,96 @@ import React from 'react';
 
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <footer className='border-top' style={{backgroundColor:"rgb(250,250,250)"}}>
+        <div className='container mt-5 '>
+            <div className='row mb-5 '>
+                <div className='col'>
+                    <img src='media/images/logo.svg' alt='logo' style={{width:"55%",paddingBottom:"1rem"}}/>
+                    <p className='text-muted' style={{fontSize:".8rem"}}>&copy 2010 - 2026, Zerodha Broking Ltd.</p>
+                    <p className='text-muted' style={{fontSize:".8rem"}}>All rights reserved.</p>
+                </div>
+                <div className='col '>
+                    <h4>Account</h4>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Open demat account</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Minor demat account</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>NRI demat account</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Commodity</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Dematerialisation</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Fund transfer</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>MTF</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Referral program</a>
+                </div>
+                <div className='col'>
+                    <h4>Support</h4>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Contact us</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Support portal</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>How to file a complaint?</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Status of your complaints</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Bulletin</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Circular</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Z-Connect blog</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Downloads</a>
+                </div>
+                <div className='col'>
+                    <h4>Company</h4>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>About</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Philosophy</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Press & media</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Careers</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Zerodha Cares (CSR)</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Zerodha.tech</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Open source</a>
+                </div>
+                <div className='col'>
+                    <h4>Quick links</h4>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Upcoming IPOs</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Brokerage charges</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Market holidays</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Economic calendar</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Calculators</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Markets</a>
+                    <a href="#" className='text-muted' style={{textDecoration:"none",display:"block",marginTop:"1rem",fontSize:".9rem"}}>Sectors</a>
+                </div>
+            </div>
+            <div className='row mt-5'>
+                <div className='col'>
+                    <div className='mt-5 text-muted' style={{fontSize:".75rem"}}>
+                    <p>Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p>
+
+                    <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p>
+
+                    <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+
+                    <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
+
+                    <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+
+                    <p>India's largest broker based on networth as per NSE. NSE broker factsheet</p>
+
+                    <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+
+                    <p>*Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                <div className="col">
+                    <div className='container text-center'>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>NSE </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>BSE </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>MCX </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>Terms & conditions </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>Policies & procedures </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>Privacy policy </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>Disclosure </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>For investor's</a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>attention </a>
+                    <a href="#" style={{textDecoration:"none",marginLeft:"1rem",color:"rgb(108, 117, 125)"}}>Investor charter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
      );
 }
 
