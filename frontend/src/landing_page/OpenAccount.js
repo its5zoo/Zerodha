fa249301f75fcef3757e824aa3c637131a4df539
Faizaan <iamrevenent007@gmail.com>
2026-03-07 21:43:02 +0530

    Navbar and OpenAccound --done

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1>Open Account</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <h2 className='mb-3'>Open a Zerodha account</h2>
+                <p className='text-muted'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button className='p-2 btn btn-primary fs-5 mt-4 my-btn' style={{width:"17%", margin:"0 auto"}}>Sign up for free</button>
+            </div>
+        </div>
      );
 }
 
