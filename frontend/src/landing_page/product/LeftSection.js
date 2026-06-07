6cc0b9acad1184a2e14bca7ea644b0398c0d03e1
Faizaan <iamrevenent007@gmail.com>
2026-06-07 14:56:39 +0530

    product and landing page done

--- frontend/src/landing_page/product/LeftSection.js
+++ frontend/src/landing_page/product/LeftSection.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-function LeftSection() {
+function LeftSection(imageURL, productName, productDescription,tryDemo, learnMore, googlePlay, appStore) {
     return ( 
-        <h1>LeftSection</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6' p-3>
+                    <img src={imageURL} />
+                </div>
+                <div className='col-6'>
+                    <h1>{productName}</h1>
+                    <p>{productDescription}</p>
+                    <div>
+                        <a href={tryDemo}>Try Demo</a>
+                        <a href={learnMore} style={{marginleft: "50px"}}/>
+                    </div>
+                    <div className='mt-3'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
